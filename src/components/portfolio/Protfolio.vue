44440afed44400f854034129cd0a8bc456320c2c
hanwong <hanationbear@gmail.com>
2018-06-25 23:26:02 +0900

    [step.1] Create Component files

--- src/components/portfolio/Protfolio.vue
+++ src/components/portfolio/Protfolio.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Portfolio</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Portfolio'
 }
 </script>
 
--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>PortfolioStock</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'PortfolioStock'
 }
 </script>
 
