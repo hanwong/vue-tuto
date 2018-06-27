6cf450ab76fdee2554207da722380248b7eb7171
hanwong <hanationbear@gmail.com>
2018-06-27 17:34:36 +0900

    renamed navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <el-menu :default-active="activeIndex" class="navbar" mode="horizontal" router="true" @select="handleSelect">
-      <el-menu-item index="1" route="/">Stock Trader</el-menu-item>
-      <el-menu-item index="2" route="/portfolio">Portfolio</el-menu-item>
-      <el-menu-item index="3" route="/stocks">Stocks</el-menu-item>
+      <el-menu-item index="1" route="/">CryptoVue </el-menu-item>
+      <el-menu-item index="2" route="/exchange">Exchange</el-menu-item>
+      <el-menu-item index="3" route="/wallet">Wallet</el-menu-item>
       <el-submenu index="5" class="navbar-menu navbar-menu__right">
         <template slot="title">Save & Load</template>
         <el-menu-item index="save">Save</el-menu-item>
         <el-menu-item index="load">Load</el-menu-item>
       </el-submenu>
-      <el-menu-item index="4" class="navbar-menu navbar-menu__right">End Day</el-menu-item>
+      <el-menu-item index="4" class="navbar-menu navbar-menu__right">Refresh</el-menu-item>
     </el-menu>
   </div>
 </template>
