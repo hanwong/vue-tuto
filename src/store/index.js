cdb6db9cd2e45c30239c78779bf02ebc987d3ece
hanwong <hanationbear@gmail.com>
2018-07-04 23:37:34 +0900

    [step.7] Create vuex wallet module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import exchange from './modules/exchange'
+import wallet from './modules/wallet'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    exchange
+    exchange,
+    wallet
   }
 })
